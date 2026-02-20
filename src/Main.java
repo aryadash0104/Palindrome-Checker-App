7cbee92aab705fec7b8bdd380738f916095ce89e
aryadash0104 <a000rya@gmail.com>
2026-02-20 11:10:34 +0530

    UC3: Palindrome check using string reverse logic

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Palindrome Checker App - UC2");
+        System.out.println("Palindrome Checker App - UC3");
 
-        String word = "madam";   // Hardcoded string
-
+        String input = "level";   // Hardcoded string
         String reversed = "";
 
-        for(int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
+        // Reverse using loop
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
         }
 
-        if(word.equals(reversed)) {
-            System.out.println(word + " is a Palindrome.");
+        System.out.println("Original String : " + input);
+        System.out.println("Reversed String : " + reversed);
+
+        // Compare using equals()
+        if (input.equals(reversed)) {
+            System.out.println("Result: It is a Palindrome.");
         } else {
-            System.out.println(word + " is NOT a Palindrome.");
+            System.out.println("Result: It is NOT a Palindrome.");
         }
     }
 }
