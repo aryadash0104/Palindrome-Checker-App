81d612e483745036ec460b665c8cfdb576daefba
aryadash0104 <a000rya@gmail.com>
2026-02-20 10:17:05 +0530

    UC1: Add welcome message module

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("==================================");
+        System.out.println("   WELCOME TO PALINDROME APP   ");
+        System.out.println("==================================");
+        System.out.println("UC1: Welcome module implemented.");
+
+    }
+}
