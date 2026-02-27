26a233b9fea4c9e29f2cfc82787a542fd83ecb96
aryadash0104 <a000rya@gmail.com>
2026-02-27 08:47:34 +0530

    UC1: Application entry & welcome message implemented

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Palindrome Checker App - UC10 (Normalized Check)");
+        System.out.println("==========================================");
+        System.out.println("   PALINDROME CHECKER APPLICATION");
+        System.out.println("==========================================");
+        System.out.println("Version : 1.0");
+        System.out.println("System Initialized Successfully.");
+        System.out.println("Ready to Process Palindrome Checks.");
 
-        String input = "A man a plan a canal Panama";
-
-        // Normalize string
-        String normalized = input.replaceAll("[^a-zA-Z]", "").toLowerCase();
-
-        boolean isPalindrome = true;
-
-        // Compare characters from both ends
-        for (int i = 0; i < normalized.length() / 2; i++) {
-
-            if (normalized.charAt(i) !=
-                    normalized.charAt(normalized.length() - 1 - i)) {
-
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        System.out.println("Input : " + input);
-        System.out.println("Normalized : " + normalized);
-        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
